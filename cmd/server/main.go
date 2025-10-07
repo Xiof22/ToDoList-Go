77b03df006101c00271052de7c2e1a5ccf3d8a84
Xiofe <azimhanmyradov10@gmail.com>
2025-10-07 21:07:12 +0500

    Setup servers and router (chi)

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,13 +1,29 @@
 package main
 
 import (
+	"fmt"
+	"github.com/Xiof22/ToDoList/config"
 	"github.com/Xiof22/ToDoList/internal/handlers"
 	"github.com/Xiof22/ToDoList/internal/repository/memory"
+	"github.com/Xiof22/ToDoList/internal/router"
 	"github.com/Xiof22/ToDoList/internal/service"
+	"net/http"
 )
 
 func main() {
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		panic(err)
+	}
+
 	m := memory.New()
 	svc := service.New(m)
 	h := handlers.New(svc)
+	r := router.New(h)
+
+	port := fmt.Sprintf(":%d", cfg.Port)
+	fmt.Printf("Serving start on %d...\n", cfg.Port)
+	if err := http.ListenAndServe(port, r); err != nil {
+		panic(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,9 @@ module github.com/Xiof22/ToDoList
 go 1.25.0
 
 require github.com/google/uuid v1.6.0 // indirect
+
+require (
+	github.com/caarlos0/env v3.5.0+incompatible // indirect
+	github.com/go-chi/chi v1.5.5 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
