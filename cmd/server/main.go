2de202ae58a0a7bbfc29084e83b389b92d2ce89f
Xiofe <azimhanmyradov10@gmail.com>
2025-10-07 21:07:12 +0500

    Setup server and router (gorilla/mux)

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+	"github.com/gorilla/mux"
 	"github.com/Xiof22/ToDoList/internal/handlers"
 	"github.com/Xiof22/ToDoList/internal/service"
 	"github.com/Xiof22/ToDoList/internal/repository"
@@ -10,4 +13,11 @@ func main() {
 	repo := repository.NewToDoRepository()
 	svc := service.NewToDoService(repo)
 	h := handler.NewToDoHandler(svc)
+	r := mux.NewRouter()
+
+	fmt.Println("Serving start...")
+	err := http.ListenAndServe(":8080", r)
+	if err != nil {
+		panic(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Xiof22/ToDoList
 
 go 1.25.0
+
+require github.com/gorilla/mux v1.8.1 // indirect
