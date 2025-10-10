c48f0bda0d3406b89b2e9c14ed56bca28b782edc
Xiofe <azimhanmyradov10@gmail.com>
2025-10-10 22:01:17 +0500

    Change storage from slice to hashmap

--- internal/repository/memory/repository.go
+++ internal/repository/memory/repository.go
@@ -13,17 +13,20 @@ var (
 )
 
 type Repository struct {
-	Tasks  []models.Task
 	mu     sync.Mutex
+	Tasks  map[int]*models.Task
 	nextID int
 }
 
 func New() *Repository {
-	return &Repository{nextID: 1}
+	return &Repository{
+		Tasks:  make(map[int]*models.Task),
+		nextID: 1,
+	}
 }
 
 func (repo *Repository) Create(title, description string) error {
-	task := models.Task{
+	task := &models.Task{
 		ID:          repo.nextID,
 		Title:       title,
 		Description: description,
@@ -33,7 +36,7 @@ func (repo *Repository) Create(title, description string) error {
 	repo.mu.Lock()
 	defer repo.mu.Unlock()
 
-	repo.Tasks = append(repo.Tasks, task)
+	repo.Tasks[repo.nextID] = task
 	repo.nextID++
 
 	return nil
@@ -43,30 +46,31 @@ func (repo *Repository) GetAll() ([]models.Task, error) {
 	repo.mu.Lock()
 	defer repo.mu.Unlock()
 
-	return repo.Tasks, nil
+	taskCount := len(repo.Tasks)
+	tasks := make([]models.Task, 0, taskCount)
+
+	for _, task := range repo.Tasks {
+		tasks = append(tasks, *task)
+	}
+
+	return sort(tasks), nil
 }
 
 func (repo *Repository) Get(id int) (*models.Task, error) {
 	repo.mu.Lock()
 	defer repo.mu.Unlock()
 
-	for index, task := range repo.Tasks {
-		if task.ID == id {
-			return &repo.Tasks[index], nil
-		}
-	}
-
-	return nil, nil
+	return repo.Tasks[id], nil
 }
 
 func (repo *Repository) Edit(id int, title, description string) error {
-	task, _ := repo.Get(id)
-	if task == nil {
-		return ErrNotFound
-	}
-
 	repo.mu.Lock()
 	defer repo.mu.Unlock()
+
+	task, found := repo.Tasks[id]
+	if !found {
+		return ErrNotFound
+	}
 
 	task.Title = title
 	task.Description = description
@@ -75,8 +79,11 @@ func (repo *Repository) Edit(id int, title, description string) error {
 }
 
 func (repo *Repository) Complete(id int) error {
-	task, _ := repo.Get(id)
-	if task == nil {
+	repo.mu.Lock()
+	defer repo.mu.Unlock()
+
+	task, found := repo.Tasks[id]
+	if !found {
 		return ErrNotFound
 	}
 
@@ -84,16 +91,16 @@ func (repo *Repository) Complete(id int) error {
 		return ErrAlreadyCompleted
 	}
 
-	repo.mu.Lock()
-	defer repo.mu.Unlock()
-
 	task.IsCompleted = true
 	return nil
 }
 
 func (repo *Repository) Uncomplete(id int) error {
-	task, _ := repo.Get(id)
-	if task == nil {
+	repo.mu.Lock()
+	defer repo.mu.Unlock()
+
+	task, found := repo.Tasks[id]
+	if !found {
 		return ErrNotFound
 	}
 
@@ -101,10 +108,8 @@ func (repo *Repository) Uncomplete(id int) error {
 		return ErrAlreadyUncompleted
 	}
 
-	repo.mu.Lock()
-	defer repo.mu.Unlock()
-
 	task.IsCompleted = false
+
 	return nil
 }
 
@@ -112,12 +117,31 @@ func (repo *Repository) Delete(id int) error {
 	repo.mu.Lock()
 	defer repo.mu.Unlock()
 
-	for index, task := range repo.Tasks {
-		if task.ID == id {
-			repo.Tasks = append(repo.Tasks[:index], repo.Tasks[index+1:]...)
-			return nil
-		}
+	_, found := repo.Tasks[id]
+	if !found {
+		return ErrNotFound
 	}
 
-	return ErrNotFound
+	delete(repo.Tasks, id)
+	return nil
+}
+
+func sort(tasks []models.Task) []models.Task {
+	for {
+		swapped := false
+
+		for i := 0; i < len(tasks)-1; i++ {
+			current := tasks[i]
+			next := tasks[i+1]
+
+			if current.ID > next.ID {
+				tasks[i], tasks[i+1] = next, current
+				swapped = true
+			}
+		}
+
+		if !swapped {
+			return tasks
+		}
+	}
 }
